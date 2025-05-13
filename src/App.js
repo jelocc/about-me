632535a4f6df9f10f65946c47c4465d663f116d2
jelozz <jelorosal9@gmail.com>
2025-05-14 02:51:32 +0800

    initial homepage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className = "header"> Hello Baby Girl</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          HELLO TEST
-        </a>
+        <div className="navBar">
+        <img src="/img/hanni baby.png" alt="Hanni" id="hanniImg" />
+          <div className="navBarTabs">
+            <a href="#" className="Home">Home</a>
+            <a href="#" className="Projects">Projects</a>
+            <a href="#" className="About">About</a>
+            <a href="#" className="Contact">Contact</a>
+          </div>
+        <div className="navBarLinks">
+          <img src="/img/hanni baby.png" alt="gitHub" id="gitHubImg"/>
+          <img src="/img/hanni baby.png" alt="linkedIn" id="linkedInImg"/>
+        </div>
+        </div>
+        <div className="heading"> 
+          <p className="headingGreet"> Hey there!
+          {String.fromCodePoint(0x1F44B, 0x1F3FC)}
+          </p>
+          <h1>I am KING <br /> an upcoming Software Engineer.</h1>
+          <br /><h4 className="introDesc"> I am a Computer Science Graduate. <br />Looking to learn a lot about Software Development. </h4>
+        </div>
       </header>
     </div>
+    
   );
 }
 
