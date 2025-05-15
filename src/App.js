d16e3cbdf502988c0418cbfc562b105cc2bf9419
jelozz <jelorosal9@gmail.com>
2025-05-16 02:41:36 +0800

    used react components for navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import './App.scss';
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Box, IconButton } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Avatar from '@mui/material/Avatar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="navBar">
-        <img src="/img/hanni baby.png" alt="Hanni" id="hanniImg" />
-          <div className="navBarTabs">
-            <a href="#" className="Home">Home</a>
-            <a href="#" className="Projects">Projects</a>
-            <a href="#" className="About">About</a>
-            <a href="#" className="Contact">Contact</a>
-          </div>
-        <div className="navBarLinks">
-          <img src="/img/hanni baby.png" alt="gitHub" id="gitHubImg"/>
-          <img src="/img/hanni baby.png" alt="linkedIn" id="linkedInImg"/>
-        </div>
-        </div>
+        <AppBar position="sticky" sx={{ 
+          backgroundColor: 'rgba(255, 0, 0, 0.2)', 
+          borderRadius: '25px', 
+          width: '70%', 
+          margin: '10px auto' 
+        }}>
+          <Toolbar sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Box sx={{ display: { xs: 'block', md: 'flex' }, gap: 2, alignItems: 'center' }}>
+              <Avatar 
+                alt="Hanni" 
+                src="/img/hanni baby.png" 
+                sx={{ width: 50, height: 50, marginRight: 2 }}
+              />
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 2 }}>
+                <Button color="inherit">Home</Button>
+                <Button color="inherit">Projects</Button>
+                <Button color="inherit">About</Button>
+                <Button color="inherit">Contact</Button>
+              </Box>
+            </Box>
+            <Box>
+              <IconButton color="inherit" href="#">
+                <GitHubIcon />
+              </IconButton>
+              <IconButton color="inherit" href="#">
+                <LinkedInIcon />
+              </IconButton>
+            </Box>
+          </Toolbar>
+        </AppBar>
         <div className="heading"> 
           <p className="headingGreet"> Hey there!
           {String.fromCodePoint(0x1F44B, 0x1F3FC)}
